2260ccf64382021a3069566e6e25b5fe2be9d518
George Chitechi <mctapplications@gmail.com>
2025-02-17 06:09:12 +0300

    Initial Commit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,10 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Aircraft Weight & Balance') }}</title>
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
     @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
-
 
 </head>
 
